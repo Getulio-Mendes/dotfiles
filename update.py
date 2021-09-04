1163896e1c428dd946f7cac3bc1c0347253979a7
Getulio-Mendes <mendes.getulio@outlook.com>
2021-09-04 14:36:07 +0000

    Auto update

--- update.py
+++ update.py
@@ -3,8 +3,13 @@ import os
 PATH = os.getenv("HOME") + "/dotfiles"
 
 file = open(r"ignore","r")
+tfile = open(r"token","r")
+
 ignore = file.read().split()
+token = tfile.read()
 file.close()
+tfile.close()
+
 itens = os.listdir(PATH)
 
 update = []
@@ -27,5 +32,5 @@ os.system("rsync  -r -v --exclude-from=./copyignore ../.local/share/fonts .")
 os.system("pacman -Qe > install.txt")
 
 os.system("git add . && git commit -m 'Auto update'")
-os.system("git push")
+os.system("printf 'Getulio-Mendes\n${}'git push".format(token))
 
